ec55f184f4fa7b70b54889b4400cc5e4af387f38
Mila-Amen <million.degefu@dci-student.org>
2025-06-16 22:28:01 +0200

    Hero image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <section className="pt-24 min-h-screen bg-primary text-center">
-      <h1 className="text-4xl md:text-6xl font-bold text-teal-700 mb-4">
-        Empowering Your Wellness Journey
-      </h1>
-      <p className="text-lg md:text-xl text-white max-w-2xl mx-auto">
-        Discover personalized health management, expert stress consultation, and
-        curated wellness resources to support your mind and body.
-      </p>
-    </section>
+    <section className="relative min-h-screen">
+    <img src="Hero.jpg" alt="" className="w-full h-full object-cover absolute inset-0 z-0" />
+  
+    <div className="relative z-10 flex items-center justify-end h-full px-6 md:px-24 min-h-screen">
+      <div className="bg-black bg-opacity-50 p-6 md:p-10 rounded-lg max-w-xl text-left">
+        <h1 className="text-4xl md:text-6xl font-bold text-teal-300 mb-4  drop-shadow-2xl">
+          Empowering Your Wellness Journey
+        </h1>
+        <p className="text-lg md:text-xl text-white">
+          Discover personalized health management, expert stress consultation,
+          and curated wellness resources to support your mind and body.
+        </p>
+      </div>
+    </div>
+  </section>
+  
   );
 }
-
