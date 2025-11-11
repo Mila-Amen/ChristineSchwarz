e18b48ccdb3a525ee51203a8c5853a054171a90d
Mila-Amen <million.degefu@dci-student.org>
2025-11-11 20:14:32 +0100

    Frontend 11.11.1

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ⚡ Important! Use relative paths in production
+  base: "/", // ⚡ Important! Use relative paths in production
   server: {
     host: true, // allows access from LAN
     port: 5173,
